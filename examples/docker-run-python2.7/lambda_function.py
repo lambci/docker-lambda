6040d5abb153da0a6e4bb819181287ee90c979e6
Gildas Le Nadan <gildas@endemic-systems.com>
2016-12-01 10:34:24 +1100

    complete runtime mock for python
    removing _get_from_env and replacing by os.environ.get. Thanks to argos83 for the tip
    
    fix white space damage
    
    update README with python2.7 runtime informations
    
    add python2.7 example

---
+++ examples/docker-run-python2.7/lambda_function.py
@@ -0,0 +1,37 @@
+from __future__ import print_function
+import os
+import sys
+
+# Just a test lambda, run with:
+# docker run -v "$PWD":/var/task lambci/lambda:python2.7
+
+def lambda_handler(event, context):
+    for arg in sys.argv:
+        print(arg)
+    print(os.getcwd())
+    print(os.path.basename(__file__))
+    for key in os.environ.keys():
+        print('{0}={1}'.format(key,os.environ[key]))
+    print(os.getuid())
+    print(os.getgid())
+    print(os.geteuid())
+    print(os.getegid())
+    print(os.getgroups())
+    print(os.umask(0222))
+
+    print(event)
+    print(context.get_remaining_time_in_millis())
+    print(context.aws_request_id)
+    if context.client_context:
+        print(context.client_context)
+    print(context.function_name)
+    print(context.function_version)
+    print(context.identity.cognito_identity_id)
+    print(context.identity.cognito_identity_pool_id)
+    print(context.invoked_function_arn)
+    print(context.log('Log this for me please'))
+    print(context.log_group_name)
+    print(context.log_stream_name)
+    print(context.memory_limit_in_mb)
+
+    return 'It works!'
