lambci/docker-lambda

commit 6a2cc55e5a2e7768de48360b61a98f4998c52939

Fix JSON Parse Error (#17)
* Fix #16

Lambda function stdout is now split on `\n` character to get the last value. This assumes the last value in stdout is the result object from the Lambda function. If it cannot be parsed, it is returned as a string. If stdout has no entries, an empty string will be returned to the caller.

* Changing result parsing catch to return undefined when it cannot parse the output.

* PR comment fixes.

index.js
@@ -53,5 +53,14 @@ module.exports = function runSync(options) {
     throw err
   }
 
-  return JSON.parse(spawnResult.stdout)
+  var stdout = spawnResult.stdout.split('\n')
+  try {
+    return JSON.parse(stdout[stdout.length - 1])
+  } catch (err) {
+    // This should return undefined and not null to indicate that either the
+    // Lambda function had not output or the output could not be parsed. Both
+    // cases should be rare and are most likely the result of an issue with the
+    // Lambda function.
+    return
+  }
 }

test.js
@@ -1,4 +1,4 @@
-require('should')
+var should = require('should')
 require('child_process').spawnSync = mockSpawnSync
 
 var dockerLambda = require('.')
@@ -75,6 +75,21 @@ result.should.eql({})
 resetMock({status: 0, stdout: 'null'})
 result = dockerLambda({returnSpawnResult: true})
 result.should.eql({status: 0, stdout: 'null'})
+
+// Should not fail if stdout contains logging
+resetMock({status: 0, stdout: 'Test\nResult\n{"success":true}'})
+result = dockerLambda()
+result.should.eql({success: true})
+
+// Should return undefined if last stdout entry cannot be parsed
+resetMock({status: 0, stdout: 'Test\nResult\nsuccess'})
+result = dockerLambda()
+should.not.exist(result)
+
+// Should return undefined when function was successful but there is no stdout
+resetMock({status: 0, stdout: ''})
+result = dockerLambda()
+should.not.exist(result)
 
 // Should throw error if spawn returns error
 resetMock({error: new Error('Something went wrong')})

commit 6b6cb2c2fe8cef98b6c758b42bb98da70e857800

Just return null if there's no stdout
This fixes an issue introduced by #17 if stdio is inherited – so just
return `null` to match the output of `JSON.parse(null)`

index.js
@@ -53,6 +53,9 @@ module.exports = function runSync(options) {
     throw err
   }
 
+  // If stdio is inherited, stdout/stderr will be null
+  if (spawnResult.stdout == null) return null
+
   var stdout = spawnResult.stdout.split('\n')
   try {
     return JSON.parse(stdout[stdout.length - 1])