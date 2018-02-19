e79eb5983649797a9013d72fda3544f8d2abfa2d
Michael Hart <michael.hart.au@gmail.com>
2018-02-19 13:33:08 -0500

    Add cmdline and parent env to dump-dotnetcore20

--- base/dump-dotnetcore20/Function.cs
+++ base/dump-dotnetcore20/Function.cs
@@ -1,81 +1,78 @@
-using System;   
+using System;
 using System.Diagnostics;
+using System.IO;
 using System.Threading.Tasks;
 
 using Amazon.Lambda.Core;
 using Amazon.S3;
 
-// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
 
 namespace dump_dotnetcore20
 {
-    public static class ShellHelper
-    {
-        public static string Bash(this string cmd)
-        {
-            var escapedArgs = cmd.Replace("\"", "\\\"");
-
-            var process = new Process()
-            {
-                StartInfo = new ProcessStartInfo
-                {
-                    FileName = "/bin/sh",
-                    Arguments = $"-c \"{escapedArgs}\"",
-                    RedirectStandardOutput = true,
-                    UseShellExecute = false,
-                    CreateNoWindow = true,
-                }
-            };
-            process.Start();
-            string result = process.StandardOutput.ReadToEnd();
-            process.WaitForExit();
-            return result;
-        }
-    }
-
     public class Function
     {
-        IAmazonS3 S3Client { get; }
-
-        /// <summary>
-        /// Default constructor. This constructor is used by Lambda to construct the instance. When invoked in a Lambda environment
-        /// the AWS credentials will come from the IAM role associated with the function and the AWS region will be set to the
-        /// region the Lambda function is executed in.
-        /// </summary>
-        public Function() => S3Client = new AmazonS3Client();
-
-        /// <summary>
-        /// Constructs an instance with a preconfigured S3 client. This can be used for testing the outside of the Lambda environment.
-        /// </summary>
-        /// <param name="s3Client"></param>
-        public Function(IAmazonS3 s3Client) => S3Client = s3Client;
-
         /// <summary>
         /// Lambda function to dump the container directories /var/lang 
         /// and /var/runtime and upload the resulting archive to S3
         /// </summary>
         /// <returns></returns>
-        public async Task<string> FunctionHandler()
+        public async Task<string> FunctionHandler(object invokeEvent, ILambdaContext context)
         {
-            var environment =  Environment.GetEnvironmentVariables();
-            foreach(var env in environment.Keys){
-                    Console.WriteLine($"{env}:{environment[env]}");
-            }
-
             string filename = "dotnetcore2.0.tgz";
             string cmd = $"tar -cpzf /tmp/{filename} --numeric-owner --ignore-failed-read /var/runtime /var/lang";
 
-            cmd.Bash();
+            Console.WriteLine($"invokeEvent: {invokeEvent}");
+            Console.WriteLine($"context.RemainingTime: {context.RemainingTime}");
+
+            Console.WriteLine("Parent cmdline:");
+            Console.WriteLine(File.ReadAllText("/proc/1/cmdline").Replace("\0", " "));
+
+            Console.WriteLine("Parent env:");
+            RunShell("xargs --null --max-args=1 < /proc/1/environ");
+
+            Console.WriteLine("This cmdline:");
+            Console.WriteLine(File.ReadAllText($"/proc/{Process.GetCurrentProcess().Id}/cmdline").Replace("\0", " "));
+
+            Console.WriteLine("This env:");
+            RunShell($"xargs --null --max-args=1 < /proc/{Process.GetCurrentProcess().Id}/environ");
+
+            Console.WriteLine($"Current working directory: {Directory.GetCurrentDirectory()}");
+
+            RunShell(cmd);
 
             Console.WriteLine("Zipping done! Uploading...");
-            await S3Client.PutObjectAsync(new Amazon.S3.Model.PutObjectRequest{
-                BucketName="lambci",
-                Key=$"fs/{filename}",
-                FilePath=$"/tmp/{filename}"
-            });
-            return "OK";
 
+            var s3Client = new AmazonS3Client();
+            var response = await s3Client.PutObjectAsync(new Amazon.S3.Model.PutObjectRequest
+            {
+                BucketName = "lambci",
+                Key = $"fs/{filename}",
+                FilePath = $"/tmp/{filename}",
+                CannedACL = S3CannedACL.PublicRead,
+            });
+
+            Console.WriteLine("Uploading done!");
+
+            return response.HttpStatusCode.ToString();
+        }
+
+        private static Process RunShell(string cmd)
+        {
+            var escapedArgs = cmd.Replace("\"", "\\\"");
+            var process = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "/bin/sh",
+                    Arguments = $"-c \"{escapedArgs}\"",
+                    UseShellExecute = false,
+                    CreateNoWindow = true,
+                }
+            };
+            process.Start();
+            process.WaitForExit();
+            return process;
         }
     }
 }
