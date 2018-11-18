ce91c6e09c5be6cb909d58bad122987ca3ede21a
Michael Hart <michael.hart.au@gmail.com>
2018-11-18 14:40:11 -0500

    Add tag-all script

---
+++ base/tag-all.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+RUNTIMES="nodejs4.3 nodejs6.10 nodejs8.10 python2.7 python3.6 java8 go1.x dotnetcore2.0 dotnetcore2.1"
+
+git tag -f latest
+
+for RUNTIME in $RUNTIMES; do
+  git tag -f $RUNTIME
+done
+
+git tag -f build
+
+for RUNTIME in $RUNTIMES; do
+  git tag -f build-${RUNTIME}
+done
