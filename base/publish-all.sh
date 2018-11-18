83a41ecb815eb613be22d0fd97909c18119b1a7c
Michael Hart <michael.hart.au@gmail.com>
2018-11-18 15:10:19 -0500

    Split publish script into normal vs build publishes

--- base/publish-all.sh
+++ base/publish-all.sh
@@ -3,10 +3,15 @@
 RUNTIMES="nodejs4.3 nodejs6.10 nodejs8.10 python2.7 python3.6 java8 go1.x dotnetcore2.0 dotnetcore2.1"
 
 docker push lambci/lambda-base
-docker push lambci/lambda-base:build
 
 for RUNTIME in $RUNTIMES; do
   echo $RUNTIME
   docker push lambci/lambda:${RUNTIME}
+done
+
+docker push lambci/lambda-base:build
+
+for RUNTIME in $RUNTIMES; do
+  echo build-${RUNTIME}
   docker push lambci/lambda:build-${RUNTIME}
 done
