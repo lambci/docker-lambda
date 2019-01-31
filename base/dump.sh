b97e284fd30a2ffa653231efb0eb7f394b5a0e7d
Michael Hart <michael.hart.au@gmail.com>
2019-01-30 20:42:45 -0500

    Fix for latest glibc changes

--- base/dump.sh
+++ base/dump.sh
@@ -4,7 +4,7 @@ RUNTIMES="node43 node610 node810 python27 python36 python37 ruby2.5 java8 go1x d
 
 for RUNTIME in $RUNTIMES; do
   echo $RUNTIME
-  aws lambda invoke --function-name "dump-${RUNTIME}" /dev/stdout
+  aws --cli-read-timeout 0 --cli-connect-timeout 0 lambda invoke --function-name "dump-${RUNTIME}" /dev/stdout
   aws logs filter-log-events --log-group-name "/aws/lambda/dump-${RUNTIME}" \
     --start-time $(node -p 'Date.now() - 5*60*1000') --query 'events[].message' --output text
 done
