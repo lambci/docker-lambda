370c234aa958e292d478058ec0f4c7bd2e469333
Brandon Shackelford <bshackelford@vsco.co>
2019-07-26 12:29:44 -0700

    Fix Node v4.3 entrypoint

--- base/native-test.sh
+++ base/native-test.sh
@@ -22,8 +22,8 @@ CMD="BUILD_ONLY=true npm install --build-from-source \
 "
 
 docker run --rm \
-  -e PATH=/usr/local/lib64/node-v4.3.x/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/opt/bin \
-  -e LD_LIBRARY_PATH=/usr/local/lib64/node-v4.3.x/lib:/lib64:/usr/lib64:/var/runtime:/var/runtime/lib:/var/task:/var/task/lib:/opt/lib \
+  -e PATH=/var/lang/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/opt/bin \
+  -e LD_LIBRARY_PATH=/var/lang/lib:/lib64:/usr/lib64:/var/runtime:/var/runtime/lib:/var/task:/var/task/lib:/opt/lib \
   -e AWS_EXECUTION_ENV=AWS_Lambda_nodejs4.3 \
   -e NODE_PATH=/var/runtime:/var/task:/var/runtime/node_modules \
   -e npm_config_unsafe-perm=true \
