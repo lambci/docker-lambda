ec3f2893bdcd97cce0eec87e962b6362fd019045
Michael Hart <michael.hart.au@gmail.com>
2019-10-02 17:52:35 -0400

    Add dotnetcore2.1 pdb

--- dotnetcore2.1/run/update_libs.sh
+++ dotnetcore2.1/run/update_libs.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 curl https://lambci.s3.amazonaws.com/fs/dotnetcore2.1.tgz | \
-  tar -xz var/runtime/Amazon.Lambda.Core.dll var/runtime/Bootstrap.dll
+  tar -xz var/runtime/Amazon.Lambda.Core.dll var/runtime/Bootstrap.dll var/runtime/Bootstrap.pdb
 
-mv ./var/runtime/*.dll ./MockBootstraps/lib/
+mv ./var/runtime/*.dll ./var/runtime/*.pdb ./MockBootstraps/lib/
 rm -rf ./var
