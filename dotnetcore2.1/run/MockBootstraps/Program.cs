51a1c3c68c631adc162bc95e9525ddf6cdd4be96
Michael Hart <michael.hart.au@gmail.com>
2019-11-09 11:14:25 -0500

    Use LambdaBootstrap and create a mock ILambdaRuntime for dotnetcore2.1

--- dotnetcore2.1/run/MockBootstraps/Program.cs
+++ dotnetcore2.1/run/MockBootstraps/Program.cs
@@ -1,11 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Reflection;
 using System.Runtime.Loader;
 using AWSLambda.Internal.Bootstrap;
-using AWSLambda.Internal.Bootstrap.Context;
-using System.Linq;
+using AWSLambda.Internal.Bootstrap.ErrorHandling;
 
 namespace MockLambdaRuntime
 {
@@ -51,40 +51,11 @@ namespace MockLambdaRuntime
                     }
                 }
 
-                var lambdaContext = new MockLambdaContext(handler, body);
-
-                var userCodeLoader = new UserCodeLoader(handler, InternalLogger.NO_OP_LOGGER);
-                userCodeLoader.Init(Console.Error.WriteLine);
-
-                var lambdaContextInternal = new LambdaContextInternal(lambdaContext.RemainingTime,
-                                                                      LogAction, new Lazy<CognitoClientContextInternal>(),
-                                                                      lambdaContext.RequestId,
-                                                                      new Lazy<string>(lambdaContext.Arn),
-                                                                      new Lazy<string>(string.Empty),
-                                                                      new Lazy<string>(string.Empty),
-                                                                      Environment.GetEnvironmentVariables());
-
-                Exception lambdaException = null;
-
-                LogRequestStart(lambdaContext);
-                try
-                {
-                    userCodeLoader.Invoke(lambdaContext.InputStream, lambdaContext.OutputStream, lambdaContextInternal);
-                }
-                catch (Exception ex)
-                {
-                    lambdaException = ex;
-                }
-                LogRequestEnd(lambdaContext);
-
-                if (lambdaException == null)
-                {
-                    Console.WriteLine(lambdaContext.OutputText);
-                }
-                else
-                {
-                    Console.Error.WriteLine(lambdaException);
-                }
+                var lambdaRuntime = new MockRuntime(handler, body);
+                LambdaBootstrap lambdaBootstrap = new LambdaBootstrap(lambdaRuntime, InternalLogger.NO_OP_LOGGER);
+                UnhandledExceptionLogger.Register();
+                lambdaBootstrap.Initialize();
+                lambdaBootstrap.Invoke();
             }
 
             // Catch all unhandled exceptions from runtime, to prevent user from hanging on them while debugging
@@ -109,12 +80,6 @@ namespace MockLambdaRuntime
                 }
             }
             throw new FileNotFoundException($"{assemblyName.Name}.dll");
-        }
-
-        /// Try to log everything to stderr except the function result
-        private static void LogAction(string text)
-        {
-            Console.Error.WriteLine(text);
         }
 
         /// <summary>
@@ -142,22 +107,6 @@ namespace MockLambdaRuntime
 
             unprocessed = unprocessedList.ToArray();
             return flagValue;
-        }
-
-        static void LogRequestStart(MockLambdaContext context)
-        {
-            Console.Error.WriteLine($"START RequestId: {context.RequestId} Version: {context.FunctionVersion}");
-        }
-
-        static void LogRequestEnd(MockLambdaContext context)
-        {
-            Console.Error.WriteLine($"END  RequestId: {context.RequestId}");
-
-            Console.Error.WriteLine($"REPORT RequestId {context.RequestId}\t" +
-                                    $"Duration: {context.Duration} ms\t" +
-                                    $"Billed Duration: {context.BilledDuration} ms\t" +
-                                    $"Memory Size {context.MemorySize} MB\t" +
-                                    $"Max Memory Used: {context.MemoryUsed / (1024 * 1024)} MB");
         }
 
         /// Gets the function handler from arguments or environment
