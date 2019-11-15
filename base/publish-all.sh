3cb8de4d177fe77f03a5e77076c19949786f2709
Michael Hart <michael.hart.au@gmail.com>
2019-11-15 14:51:02 -0500

    Read repository passphrase at start and reuse for all publishes

--- base/publish-all.sh
+++ base/publish-all.sh
@@ -2,7 +2,12 @@
 
 RUNTIMES="provided go1.x nodejs4.3 nodejs6.10 nodejs8.10 nodejs10.x python2.7 python3.6 python3.7 ruby2.5 java8 dotnetcore2.0 dotnetcore2.1"
 
+echo -n "Enter repository passphrase: "
+read -s DOCKER_CONTENT_TRUST_REPOSITORY_PASSPHRASE
+echo
+
 export DOCKER_CONTENT_TRUST=1
+export DOCKER_CONTENT_TRUST_REPOSITORY_PASSPHRASE
 
 docker push lambci/lambda-base
 docker push lambci/lambda-base-2
