473d446ebd09e61d59b6fc32bfa8030cba422986
Michael Hart <michael.hart.au@gmail.com>
2019-11-18 17:30:40 -0500

    Add new runtimes to dump.sh

--- base/dump.sh
+++ base/dump.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-RUNTIMES="node43 node610 node810 node10x python27 python36 python37 ruby25 java8 go1x dotnetcore20 dotnetcore21 provided"
+RUNTIMES="node43 node610 node810 node10x node12x python27 python36 python37 python38 ruby25 java8 java11 go1x dotnetcore20 dotnetcore21 provided"
 
 for RUNTIME in $RUNTIMES; do
   echo $RUNTIME
