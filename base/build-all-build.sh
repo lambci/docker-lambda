5e4f9b565e84e4f57ed7a09d9c7eb424c06cd9c8
Gábor Lipták <gliptak@gmail.com>
2020-09-25 09:54:52 -0400

    Add Github Actions build
    Signed-off-by: Gábor Lipták <gliptak@gmail.com>

---
+++ base/build-all-build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+RUNTIMES="provided go1.x nodejs4.3 nodejs6.10 nodejs8.10 python2.7 python3.6 python3.7 ruby2.5 java8 dotnetcore2.0 dotnetcore2.1 provided.al2 nodejs10.x nodejs12.x python3.8 ruby2.7 java8.al2 java11 dotnetcore3.1"
+
+TOP_DIR="${PWD}/.."
+
+for RUNTIME in $RUNTIMES; do
+  echo build-${RUNTIME}
+
+  cd ${TOP_DIR}/${RUNTIME}/build
+
+  docker build -t lambci/lambda:build-${RUNTIME} .
+done
